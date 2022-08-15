4ce7518e850f7590ad7e8e4941c5cc20b860b74a
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-08-15 08:24:18 -0700

    coding-concepts.js responses

--- coding-concepts.js
+++ coding-concepts.js
@@ -8,10 +8,10 @@
 // --------------------1) What will this log?
 
 const cohort = "Echo 2022"
-// console.log(cohort.split(""))
+console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will log a new array in which each element, at each index position, is seperated into individual elements. ['E', 'c', 'h', 'o', ' ', '2', '0', '2', '2']
+// b) Verify and explain: split() method splits a string into a new array of individual strings, without changing the original string. because the string "Echo 2022" has nine elements, the new array will split each element into an individual string.
 
 
 // --------------------2) What will this log?
@@ -19,28 +19,28 @@ const cohort = "Echo 2022"
 const greeter = (name) => {
   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: this will log undefined because "Learn Student" has not been declared.
+// b) Verify and explain: the function is working as designed, however, because the argument "LEARN Student" has not been declared, the console.log(greeter("LEARN Student")) will return undefined.
 
 
 // --------------------3) What will this log?
 
 const multipliedByTwo = [4, 5, 6, 7, 8].map(number => number * 2)
-// console.log(multipliedByTwo)
+console.log(multipliedByTwo)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:[ 8, 10, 12, 14, 16 ]
+// b) Verify and explain: .map will go through each element in the specified array and multiply each number by a factor of 2. A new array will return with the the product of [i]*2
 
 
 // --------------------4) What will this log?
 
 const onlyOdds = [11, 12, 13, 14, 15].filter(number => number % 2 !== 0)
-// console.log(onlyOdds)
+console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [11, 13,15]
+// b) Verify and explain: the .filter method will return a new array of elements that passed the test provided by the function. in this case only the odd numbers from the original function.
 
 
 // --------------------5) What will this log?
@@ -51,10 +51,10 @@ const myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: the console.log will return the string "Javascript".
+// b) Verify and explain: the array languages has two index positions, and the console.log is asking for the [0] index. so, "Javascript" will return.
 
 
 // --------------------STRETCH: What will this log?
@@ -67,7 +67,7 @@ class Learn {
   }
 }
 const learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Learn { student: 'George', cohort: 'Echo', year: 2022 }
+// b) Verify and explain: within the class Learn, the contsructor function witch contains the this. method which referes to an object. the variable learnStudent is passed through the console.log requesting a return of the specified name, along with the objects contained within the function constructor.
